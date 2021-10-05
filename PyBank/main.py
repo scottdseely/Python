7f57c58466edfbeddb5a8efcf7ca4bbefd38a480
Scott Seely <scott.seely@sfsoma.com>
2021-10-04 19:49:35 -0700

    updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,3 +8,19 @@
 
 
 
+#Calculate the total number of months
+
+#Calc the total net profit/loss
+
+
+#changes in P&L for each month
+
+#Calc the avg change in P&L
+
+#Converted list to dictionary to use in analysis
+
+
+#Calc largest increase/decrease in P&L
+
+#Summary
+
